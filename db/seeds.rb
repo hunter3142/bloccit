cd814c9400f615b74d87f5dde157b7da478f5e78
Mathew <hunter3142@gmail.com>
2017-07-17 22:18:08 -0400

    Checkpoint 27 rails posts users: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
 require 'random_data'
+# Create Users
+5.times do
+  User.create!(
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
 15.times do
    	Topic.create!(
@@ -11,6 +21,7 @@ topics = Topic.all
 # Create Posts
 50.times do
    	Post.create!(
+      user:   users.sample,
    		topic: topics.sample,
      	title:  RandomData.random_sentence,
      	body:   RandomData.random_paragraph
@@ -25,8 +36,15 @@ posts = Post.all
      	body: RandomData.random_paragraph
    	)
 end
+
+user = User.first
+user.update_attributes!(
+  email: 'hunter3142@gmail.com', # replace this with your personal email
+  password: 'helloworld'
+)
  
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
