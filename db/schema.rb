cced1a901cdefc39fdab208c27dced01698b44ab
Mathew <hunter3142@gmail.com>
2017-07-20 00:18:24 -0400

    Checkpoint 29 rails comments: completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718031651) do
+ActiveRecord::Schema.define(version: 20170720034438) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -19,10 +19,12 @@ ActiveRecord::Schema.define(version: 20170718031651) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.integer  "sponsored_post_id"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
   add_index "comments", ["sponsored_post_id"], name: "index_comments_on_sponsored_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
